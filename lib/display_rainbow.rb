b0b03222d0fd30e3cb1928e95efa3cf9dae27bc7
John Tuttle <thejwt@gmail.com>
2017-09-19 16:47:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(arrayColors)
-  puts "R: #{arrayColors[0]}, O: #{arrayColors[1]}, Y: #{arrayColors[2]}, G: #{arrayColors[3]}, B: #{arrayColors[4]}, I: #{arrayColors[5]}, V: #{arrayColors[6]}}"
+  puts "R: #{arrayColors[0]}, O: #{arrayColors[1]}, Y: #{arrayColors[2]}, G: #{arrayColors[3]}, B: #{arrayColors[4]}, I: #{arrayColors[5]}, V: #{arrayColors[6]})}"
 end
